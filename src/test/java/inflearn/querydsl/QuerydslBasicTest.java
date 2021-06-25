158774f2feae2d2774edd1733bc9f1582a2e25e6
jinyedo <whereyedo@naver.com>
2021-06-25 18:51:33 +0900

    Querydsl 과 JPQL 비교하기

---
+++ src/test/java/inflearn/querydsl/QuerydslBasicTest.java
@@ -0,0 +1,75 @@
+package inflearn.querydsl;
+
+import com.querydsl.jpa.JPQLQueryFactory;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import inflearn.querydsl.entity.Member;
+import inflearn.querydsl.entity.QMember;
+import inflearn.querydsl.entity.Team;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+public class QuerydslBasicTest {
+
+    @Autowired
+    EntityManager em;
+
+    @BeforeEach
+    public void before() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
+
+    @Test // JPQL 테스트
+    public void startJPQL() {
+        // member1을 찾아라
+        Member findMember = em.createQuery("select m from Member m where m.username = :username", Member.class)
+                .setParameter("username", "member1")
+                .getSingleResult();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test // Querydsl 테스트
+    public void startQuerydsl() {
+        JPQLQueryFactory queryFactory = new JPAQueryFactory(em); // EntityManager 로 QueryFactory 새성
+        QMember m = new QMember("m"); // 어떤 QMember 인지 구분하기 위해 'm' 이라는 이름으로 설정
+
+        Member findMember = queryFactory
+                .select(m)
+                .from(m)
+                .where(m.username.eq("member1")) // PreparedStatement 를 통한 자동 파라미터 바인딩 처리
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    /* JPQL */
+    // 파라미터 값들을 문자 + 방식으로 사용하기 때문에 SQL Injection 공격을 받을 수 있음
+    // 문자로 작성 하기 때문에 실행 후 메서드를 호출했을떄 오류를 확인할 수 있음
+
+    /* Querydsl */
+    // PreparedStatement 를 통한 자동 파라미터 바인딩 처리 방식을 사용하기 때문에 SQL Injection 공격을 막을 수 있음
+    // 코드로 작성하기 컴파일 시점에서 오류를 확인할 수 있음 - 문법 오류 방지
+
+}
